21b5ed138718458908bb96d9040f93c0637f57f1
Melnyk Zoriana <48826458+zoriana-melnyk@users.noreply.github.com>
2021-07-28 11:57:33 +0300

    Initial

---
+++ src/components/info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Info extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h2>This is weather app</h2>
+                <p>Cheack wasther in your city</p>
+            </div>
+
+        );
+    }
+}
+
+export default Info;
---
+++ src/components/weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Weather extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                Componet with weather info
+            </div>
+
+        );
+    }
+}
+
+export default Weather;
