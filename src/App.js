ee56addc185927b7b31c0cca0f469ca2d71b5b4c
Melnyk Zoriana <48826458+zoriana-melnyk@users.noreply.github.com>
2021-07-28 21:35:11 +0300

    added weather API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Info from "./components/info";
 import Form from "./components/form";
 import Weather from "./components/weather";
 
+API_KEY = "ffc1d3783f226e08697ca0a486d51551";
+
 class App extends React.Component{
+
+  gettingWeather = async () => {
+    const API_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    const data = API_url.json();
+    console.log(data);
+  }
 
   render(){
     return(
