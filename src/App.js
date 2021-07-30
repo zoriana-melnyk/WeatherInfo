f0da50ba96391c1fc4cf571f35771f01904db2a3
Melnyk Zoriana <48826458+zoriana-melnyk@users.noreply.github.com>
2021-07-30 19:00:03 +0300

    added weather info in form

--- src/App.js
+++ src/App.js
@@ -7,12 +7,41 @@ const API_KEY = "ffc1d3783f226e08697ca0a486d51551";
 
 class App extends React.Component{
 
+  state ={
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
   gettingWeather = async (e) => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const API_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await API_url.json();
-    console.log(data);
+    let city = e.target.elements.city.value;
+    
+    if(city){
+      const API_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await API_url.json();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        error: undefined
+      });
+    }else{
+      this.setState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: "Please, enter the city"
+      });
+    }
   }
 
   render(){
@@ -20,7 +49,14 @@ class App extends React.Component{
       <div>
         <Info />
         <Form weatherMethod={this.gettingWeather}/>
-        <Weather />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
