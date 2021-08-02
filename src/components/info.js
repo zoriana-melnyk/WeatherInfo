8ab166804078196f405e40fe3ef520e5b8ee5831
Melnyk Zoriana <48826458+zoriana-melnyk@users.noreply.github.com>
2021-08-02 14:16:41 +0300

    update css styles

--- src/components/info.js
+++ src/components/info.js
@@ -3,7 +3,7 @@ import React from "react";
 const Info = () =>(
     <div>
         <h2>This is weather app</h2>
-        <p>Check wasther in your city</p>
+        <p>Check weather in your city</p>
     </div>
 );
 
